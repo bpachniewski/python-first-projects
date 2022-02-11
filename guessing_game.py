813fa7a55cf593b55de886759614dbc93a58ef03
bartosz <bpachniewski@gmail.com>
2022-02-12 00:12:08 +0100

    guessing game

---
+++ guessing_game.py
@@ -0,0 +1,34 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print('Sorry, guess again. Too low.')
+        elif guess > random_number:
+            print('Sorry, guess again. Too high.')
+
+    print(f'Yay, congrats. You have guessed the number {random_number} correctly!!')
+
+# think of an integer within given max_value and keep instructing the computer!
+def computer_guess(max_value:int):
+    low = 1
+    high = max_value
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Nice! The computer guessed your number, {guess}, correctly!')
+
+
+computer_guess(15000)
